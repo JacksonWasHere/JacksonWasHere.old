4bf4f3a6cbf6c760ca349de1e3473b9e994e8a95
Jackson Weidmann <Jackson.weidmann@gmail.com>
2017-08-06 14:47:31 -0700

    removed stuff

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,14 @@
-function setup() {
+function fireWork(){
+  this.update(){
+    rect(0,0,10,10);
+  }
+}
 
+function setup() {
+  canvas(width,height);
+  rect(0,0,10,10);
 }
 
 function draw() {
-  
+
 }
